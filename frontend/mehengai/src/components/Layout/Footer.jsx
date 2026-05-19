8a0b480acd79ce28f9bc274060fa360414f7d792
0ManasVerma0 <manasverma1357@gmail.com>
2026-05-20 03:16:46 +0530

    add minor change in footer section

--- frontend/mehengai/src/components/Layout/Footer.jsx
+++ frontend/mehengai/src/components/Layout/Footer.jsx
@@ -4,7 +4,10 @@ export default function Footer() {
 			<div className="mx-auto max-w-7xl rounded-[28px] border-4 border-black bg-black px-5 py-6 text-white shadow-[8px_8px_0_#fff]">
 				<p className="text-sm font-black uppercase tracking-[0.18em]">Mehengai</p>
 				<p className="mt-2 max-w-3xl text-sm leading-6 text-white/85">
-					All the Data is taken from MOSPI and Labour Bureau of India. <br /> For reference only — not financial advice
+					All the Data is taken from MOSPI and Labour Bureau of India. <br /> For reference only — not financial advice <br /> Developed By - Manas Verma <br />
+					<a href="https://github.com/0ManasVerma0" target="_blank">
+      					GitHub Profile Link 0ManasVerma0
+    				</a>
 				</p>
 			</div>
 		</footer>
